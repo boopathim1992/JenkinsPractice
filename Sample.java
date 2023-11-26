d1cefe60c8298dc1bef6deec414b56c467983ecc
boopathim1992 <151808839+boopathim1992@users.noreply.github.com>
2023-11-26 12:39:06 +0530

    Sample.java

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class HelloGuys {
+  public static void main(String[] args) {
+    System.out.println("Hello Agni");
+    System.out.println("Hello Riya");
+    System.out.println("Hello Duck");
+    System.out.println("Hello Arya");
+  }
+}
